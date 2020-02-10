edd2dbf81c09154dab3a19b64b2d423b7096baf9
User Induction <induction@dsone.3ds.com>
2020-02-10 13:38:27 +0530

    mynotes

--- test.js
+++ test.js
@@ -1,42 +1,39 @@
 //var g_a =1;
 //g_b =2;
 
-function add(a,b)
-{
-    var c = a+b;
+function add(a, b) {
+    var c = a + b;
     return c;
 }
 
-function subtract(a,b)
-{
-    var c = a-b;
+function subtract(a, b) {
+    var c = a - b;
     return c;
 
 }
 
-function multiply(a,b)
-{
-    var c = a*b;
+function multiply(a, b) {
+    var c = a * b;
     return c;
 }
 
-function divide(a,b)
-{
-    var c = a/b;
+function divide(a, b) {
+    var c = a / b;
     return c;
 }
 
 
-var x =5;
-var y =7;
-var addresult = add(x,y);
-var subresult = subtract(x,y);
-var mulresult = multiply(x,y);
-var divresult = divide(x,y);
+var x = 5;
+var y = 7;
+var addresult = add(x, y);
+x = 8; y = 0;
+var subresult = subtract(x, y);
+x = 9; y = 0;
+var mulresult = multiply(x, y);
+x = 18; y = 0;
+var divresult = divide(x, y);
 
-console.log (addresult);
-console.log (subresult);
-console.log (mulresult);
-console.log (divresult);
-
-
+console.log(addresult);
+console.log(subresult);
+console.log(mulresult);
+console.log(divresult);
