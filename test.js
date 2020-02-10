1bbc637884ba5052c3be7f36d6b168a1746d4d0e
User Induction <induction@dsone.3ds.com>
2020-02-10 12:00:16 +0530

    4func

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var g_a =1;
-g_b =2;
+//var g_a =1;
+//g_b =2;
 
 function add(a,b)
 {
@@ -7,8 +7,36 @@ function add(a,b)
     return c;
 }
 
+function subtract(a,b)
+{
+    var c = a-b;
+    return c;
+
+}
+
+function multiply(a,b)
+{
+    var c = a*b;
+    return c;
+}
+
+function divide(a,b)
+{
+    var c = a/b;
+    return c;
+}
+
+
 var x =5;
 var y =7;
-var z = add(x,y);
-console.log (z);
+var addresult = add(x,y);
+var subresult = subtract(x,y);
+var mulresult = multiply(x,y);
+var divresult = divide(x,y);
+
+console.log (addresult);
+console.log (subresult);
+console.log (mulresult);
+console.log (divresult);
+
 
