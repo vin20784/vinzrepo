f726db6eba44449ed84bde2630b729dffff827bc
User Induction <induction@dsone.3ds.com>
2020-02-12 14:10:50 +0530

    bank system

--- BankSys/BAccount.js
+++ BankSys/BAccount.js
@@ -1,14 +1,31 @@
-class Account { 
 
-    constructor() { 
-        if (this.constructor == Account) {
+
+class Account {
+
+    constructor(AccNumber, AccHoldName, balance, transaction) {
+
+        /*if (this.constructor == Account) {
             throw new Error("Abstract classes can't be instantiated.");
-        }
+        }*/
+        this.AccNumber = AccNumber;
+        this.AccHoldName = AccHoldName;
+        this.balance = balance;
+        this.transaction = transaction;
+    }
+    deposit(AmtDep) {
+        //throw new Error("Method 'deposit()' must be implemented.");
+        //balance += AmtDep;
+        console.log("test");
+    }
+
+    withdraw(AmtWithdraw) {
+
     }
 };
 
-class veju extends Account{ 
 
-    constructor() { }
+module.exports.bAcc = Account;
 
-}
+
+
+
---
+++ BankSys/SavingAcc.js
@@ -0,0 +1,30 @@
+
+const accB = require('./BAccount');
+
+//let obj = new accB.bAcc();
+//obj.deposit(34);
+
+class SavingAcc extends accB.bAcc {
+
+    constructor(AccNumber, AccHoldName, balance, transaction) {
+        super(AccNumber, AccHoldName, balance, transaction);
+        this.minBal = 100;// min bal 100 rs
+
+    }
+    deposit(AmtDep) {
+         balance += AmtDep;
+    }
+
+    withdraw(AmtWithdraw) {
+
+
+    }
+
+};
+
+let SAcc = new SavingAcc(26354, 'vinz', 10000);
+
+console.table(SAcc);
+
+SAcc.deposit(50);
+
